44d002eef45ca51f4fcbc5859489497a0281d158
Quim Picó Mora <quimpm99@gmail.com>
2021-06-17 11:09:09 +0200

    added travis

--- setup.py
+++ setup.py
@@ -1,18 +1,28 @@
 from setuptools import setup, find_packages
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 setup(
     name="youtube_discusion_tree_api",
-    version="0.0.1",
+    version="1.0.0",
     author="Quim10^-12",
     author_email="quimpm99@gmail.com",
-    url="https://github.com/quimpm",
-    description="This is a python API that allows you to obtain the discusion that occurs on the comments of a Youtube video as a tree structure. It also controls the quota usage that consumes your implementation over Youtube Data Api through this library, and allows you to represent and serialize the discusion tree.",
-    packages=find_packages(),
+    url="https://github.com/quimpm/youtube_discussion_tree",
+    description="This is a python API that allows you to obtain the discusion that occurs on the comments of a Youtube video as a tree structure.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    packages=find_packages(exclude=("tests",)),
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=["click", "pytz"],
-    entry_points={"console_scripts": ["timechecker = src.main:main"]},
+    install_requires=["requests", "youtube_transcript_api", "transformers", "torch", "torchvision",  "torchaudio", "treelib", "nlt"],
 )
--- youtube_discussion_tree_api/__init__.py
+++ youtube_discussion_tree_api/__init__.py
@@ -1,1 +1,3 @@
 from ._api import YoutubeDiscusionTreeAPI
+
+__version__ = "1.0.0"
